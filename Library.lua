da2a3ec4b7de74dd2a5170493fe9982bf5285c3c
1AreYouMental110 <areyoumental110@gmail.com>
2026-03-14 13:52:47 -0500

    Add comment in Library.lua

--- Library.lua
+++ Library.lua
@@ -15,7 +15,7 @@ local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
 local ScreenGui = Instance.new('ScreenGui');
 ProtectGui(ScreenGui);
-
+-- Boiiii
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
 ScreenGui.Parent = CoreGui;
 
